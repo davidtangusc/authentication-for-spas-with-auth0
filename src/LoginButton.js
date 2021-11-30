25731969b6153376b11798d3f387c2f457e2d1a9
David Tang <dtang859@gmail.com>
2021-11-29 22:05:16 -0800

    1. Login and Logout

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,6 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function LoginButton() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
-    <button type="button" className="btn btn-default">
+    <button
+      type="button"
+      className="btn btn-default"
+      onClick={() => loginWithRedirect()}
+    >
       Log In
     </button>
   );
--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -1,6 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function LogoutButton() {
+  const { logout } = useAuth0();
+
   return (
-    <button type="button" className="btn btn-default">
+    <button
+      type="button"
+      className="btn btn-default"
+      onClick={() => logout({ returnTo: window.location.origin })}
+    >
       Log Out
     </button>
   );

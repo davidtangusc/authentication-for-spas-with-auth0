0517f5b9b886d1df413536ab76ee3ef527093be0
David Tang <dtang859@gmail.com>
2021-11-29 22:13:42 -0800

    3. Display user information in the Profile component

--- src/Profile.js
+++ src/Profile.js
@@ -1,3 +1,24 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function Profile() {
-  return <div>Profile placeholder</div>;
+  const { user } = useAuth0();
+
+  // console.log(user);
+
+  return (
+    <>
+      {user && (
+        <div>
+          <h1>{user.name}</h1>
+          <p>{user.email}</p>
+          <img
+            src={user.picture}
+            alt={user.name}
+            className="rounded-circle"
+            width="150"
+          />
+        </div>
+      )}
+    </>
+  );
 }
